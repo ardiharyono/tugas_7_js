3c1093d5936292a3de90623f8ebcf7c306d732e6
Ardi Haryono <ardiharyono26@gmail.com>
2021-10-16 17:15:01 +0700

    Tugas 7 JS
    Tugas ini kami kumpulkan untuk Niomic

---
+++ tugas7.js
@@ -0,0 +1,13 @@
+tinggibadan = [156,157,158,159,160,161,162,163,164,165]
+
+// perulangan biasa
+console.log("perulangan biasa");
+for(i=0;i<tinggibadan.length;i++) {
+  console.log(tinggibadan[i])
+}
+
+// perulangan for of
+console.log("perulangan for of");
+for(let x of tinggibadan) {
+  console.log(x)
+}
